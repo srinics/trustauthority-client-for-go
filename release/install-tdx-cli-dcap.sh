7750a332ab418581fefe68e038b5a1e224a40b2d
Srinivasan Rajendran <srinivasan.rajendran@intel.com>
2024-02-12 23:07:19 -0800

    Modified script

--- release/install-tdx-cli-dcap.sh
+++ release/install-tdx-cli-dcap.sh
@@ -26,7 +26,6 @@ print_error_and_exit()
 trap 'installation_intrupted' 1 2 3 6
 
 readonly OS=$(uname)
-#TODO need to change repo url
 readonly REPO_URL="srinics/trustauthority-client-for-go"
 readonly RAW_MAKEFILE="https://raw.githubusercontent.com/${REPO_URL}/main/tdx-cli/Makefile"
 if [ -z "${CLI_VERSION}" ]; then
@@ -36,18 +35,15 @@ readonly INSTALL_DIRECTORY=/usr/bin
 readonly OS_DISTRO=$(cat /etc/os-release  | grep "^ID=" | sed -e "s/^ID=\(\s\+\)\?\(.*\)\(\s\+\)\?$/\2/g" -e "s/\"//g")
 readonly OS_DISTRO_VERSION=$(cat /etc/os-release  | grep "^VERSION_ID=" | tr -d '"' | sed -e "s/^VERSION_ID=\(\s\+\)\?\(.*\)\(\s\+\)\?$/\2/g" -e "s/\"//g")
 readonly TAR_NAME="trustauthority-cli-${OS_DISTRO}-${CLI_VERSION}.tar.gz"
-readonly README_LINK="https://github.com/${REPO_URL}/tree/master/tdx-cli"
+readonly README_LINK="https://github.com/${REPO_URL}/tree/master/tdx-cli#usage"
 readonly CLI_BIN=$(curl -s ${RAW_MAKEFILE}  | grep "^APPNAME.*=" | sed -e "s/APPNAME.*=\(\s\+\)\?//g")
 readonly URL="https://github.com/${REPO_URL}/releases/download/${CLI_VERSION}/${TAR_NAME}"
 readonly CLI_NAME="Intel Trust Authority Client for ${OS_DISTRO^^}"
-
-
 
 installation_intrupted()
 {
     printf "\n%b%s Installation interrupted by signal !!%b\n\n" "${CODE_ERROR}" "${CLI_NAME}" "${CODE_NC}"
 }
-
 
 if [ "${OS_DISTRO}" == "ubuntu" ]; then
     if [ "${OS_DISTRO_VERSION}" == "20.04" ]; then
@@ -67,33 +63,27 @@ if [ "${OS_DISTRO}" == "ubuntu" ]; then
         printf "\n%bUnsupported Linux Distribution - %s-%s %b\n\n" "${CODE_ERROR}" "${OS_DISTRO}" "${OS_DISTRO_VERSION}" "${CODE_NC}"
         print_error_and_exit
     fi
+    apt-get update -y > /dev/null || print_error_and_exit
+    apt-get -qq install libtdx-attest -y > /dev/null || print_error_and_exit
 elif [ "${OS_DISTRO}" == "rhel" ] && [ "${OS_DISTRO_VERSION}" = "9.2" ]; then
     pushd /tmp > /dev/null
     wget -q0 - https://download.01.org/intel-sgx/latest/linux-latest/distro/${OS_DISTRO}${OS_DISTRO_VERSION}-server/sgx_rpm_local_repo.tgz || print_error_and_exit
-    wget -q0 - https://download.01.org/intel-sgx/latest/dcap-latest/linux/SHA256SUM_dcap_1.19.cfg || print_error_and_exit
-    sha256sum sgx_rpm_local_repo.tgz || print_error_and_exit
     tar xvf sgx_rpm_local_repo.tgz || print_error_and_exit
     yum-config-manager --add-repo file:/tmp/sgx_rpm_local_repo || print_error_and_exit
-    dnf --nogpgcheck install libtdx-attest-devel -y > /dev/null || print_error_and_exit
+    dnf --nogpgcheck install libtdx-attest -y > /dev/null || print_error_and_exit
     popd
-elif ([[ "${OS_DISTRO}" == "opensuse"* ]] || ["${OS_DISTRO}" == "sles" ]] ) && [[ "${OS_DISTRO_VERSION}" = "15"* ]]; then
+elif ([[ "${OS_DISTRO}" == "opensuse"* ]] || ["${OS_DISTRO}" == "sles" ]] ) && [[ "${OS_DISTRO_VERSION}" = "15.4" ]]; then
     pushd /tmp > /dev/null
     wget -q0 - https://download.01.org/intel-sgx/latest/linux-latest/distro/suse${OS_DISTRO_VERSION}-server/sgx_rpm_local_repo.tgz || print_error_and_exit
-    wget -q0 - https://download.01.org/intel-sgx/latest/dcap-latest/linux/SHA256SUM_dcap_1.19.cfg || print_error_and_exit
-    sha256sum sgx_rpm_local_repo.tgz || print_error_and_exit
     tar xvf sgx_rpm_local_repo.tgz || print_error_and_exit
     zypper addrepo /opt/intel/sgx_rpm_local_repo /tmp/sgx_rpm_local_repo || print_error_and_exit
-    zypper --no-gpg-checks install libtdx-attest-devel -y  > /dev/null || print_error_and_exit
+    zypper --no-gpg-checks install libtdx-attest -y  > /dev/null || print_error_and_exit
     popd
 else 
     printf "\n%bUnsupported Linux Distribution - %s-%s %b\n\n" "${CODE_ERROR}" "${OS_DISTRO}" "${OS_DISTRO_VERSION}" "${CODE_NC}"
     print_error_and_exit
 fi
 
-if [ "${OS_DISTRO}" == "ubuntu" ]; then
-    apt-get update -y > /dev/null || print_error_and_exit
-    apt-get -qq install libtdx-attest-dev -y > /dev/null || print_error_and_exit
-fi
 
 printf "\n%s installation started.........\n\n" "${CLI_NAME}"
 
